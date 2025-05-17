b4172f5431e0915f814a295163fb3d4aabf7c541
Eric Corbisier <contact@corbisier.fr>
2025-05-17 16:34:47 +0200

    Eric_Corbisier//Mise à jour de gitignore

---
+++ install_windows.bat
@@ -0,0 +1,34 @@
+@echo off
+cls
+
+cd install_windows
+set i=2
+
+if %i% NEQ 1 (
+    npm run start
+) else (
+
+    @REM Initialiser un projet npm si le fichier package.json n'existe pas déjà
+    if 1 == 1 (
+        echo Initialisation du projet npm...
+        npm init -y
+
+        @REM Installer Express si ce n'est pas déjà installé
+        if 1 == 1 (
+            echo Installation en cours...
+            npm i express socket.io yaml fs node-notifier open child_process uuid
+
+            if 1 == 1 (
+                echo Installation des dépendances dev...
+                npm i --save-dev typescript nodemon concurrently @types/node @types/yaml @types/node-notifier
+
+                @REM Lancer le script Node.js
+                echo Démarrage de l'application Node.js...
+                @REM npm run dev
+            )
+        )
+    )
+    pause
+)
+
+pause
